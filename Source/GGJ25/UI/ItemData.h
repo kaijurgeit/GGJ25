b4d6e5588317835277f3e344ead0653ae15432ca
Vanessa Krohn <vanessa.krohn@gmx.net>
2025-01-26 00:39:37 +0100

    Feat: Item UI

---
+++ Source/GGJ25/UI/ItemData.h
@@ -0,0 +1,16 @@
+/*#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FItemData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UTexture2D* ItemIcon;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	bool bIsActive;
+};*/
